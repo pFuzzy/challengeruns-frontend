384d50008c2c2251b14357ee12cb5865145cd908
pFuzzy <p.fazii888@gmail.com>
2020-11-13 10:41:53 -0800

    main page now uses contentnavbar

--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import {
-  Content,
-  Contentnav,
-  Contentbutton,
-} from '../styled-components/Content';
+import { Content } from '../styled-components/Content';
+import Contentnavbar from './Contentnavbar';
 
 const Mainpage = () => {
   return (
     <Content>
-      <Contentnav>
-        <Contentbutton to='/games'>Games</Contentbutton>
-        <Contentbutton to='/categories'>Categories</Contentbutton>
-        <Contentbutton to='/leaderboards'>Leaderboards</Contentbutton>
-      </Contentnav>
+      <Contentnavbar />
     </Content>
   );
 };
