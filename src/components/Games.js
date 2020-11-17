3198456355a656304ac8bb0be1c8f8d50c19a149
pFuzzy <p.fazii888@gmail.com>
2020-11-17 12:42:49 -0800

    install axios and make basic connection to backend

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Content } from '../styled-components/Content';
 import Contentnavbar from './Contentnavbar';
+import axios from 'axios';
 
 const Games = () => {
+  const [games, setGames] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:8080/games').then((res) => {
+      setGames(res.data);
+    });
+  }, []);
+  console.log(games);
   return (
     <Content>
       <Contentnavbar />
+      {games.map((game) => {
+        return <div>{game.title}</div>;
+      })}
     </Content>
   );
 };
