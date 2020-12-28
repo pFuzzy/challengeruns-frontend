ab2441af6d25db3cae1a580393d6b6d5cdf644d7
pFuzzy <p.fazii888@gmail.com>
2020-12-28 16:21:28 +0100

    add basic styling and validation

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,32 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Content } from '../styled-components/Content';
 import axios from 'axios';
+import styled from 'styled-components';
 
 const Register = () => {
+  const [status, setStatus] = useState('');
+
   const register = (e) => {
     e.preventDefault();
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
     let email = document.getElementById('email').value;
     let postData = { username: username, password: password, email: email };
-
-    console.log(postData);
     axios.post('http://localhost:8080/register', postData).then((res) => {
-      console.log(res.data);
+      setStatus(res.data);
     });
   };
 
+  const RegisterForm = styled.form`
+    width: 50%;
+    margin: auto;
+    margin-top: 5vh;
+    text-align: center;
+    border: 1px solid black;
+  `;
+
   return (
     <Content>
-      <form onSubmit={register}>
-        <input type='text' id='username' placeholder='username' />
+      <RegisterForm onSubmit={register}>
+        <label htmlFor='email'>E-mail: </label>
+        <input
+          type='email'
+          name='email'
+          id='email'
+          required='true'
+          autoComplete='off'
+          placeholder='e-mail'
+        />
         <br />
-        <input type='password' id='password' placeholder='password' />
+        <label htmlFor='username'>Username: </label>
+        <input
+          type='text'
+          name='username'
+          id='username'
+          required='true'
+          minLength='6'
+          maxLength='16'
+          autoComplete='off'
+          placeholder='username'
+        />
         <br />
-        <input type='email' id='email' placeholder='email' />
+        <label htmlFor='password'>Password: </label>
+        <input
+          type='password'
+          name='password'
+          id='password'
+          required='true'
+          minLength='6'
+          maxLength='16'
+          placeholder='password'
+        />
         <br />
         <button type='submit'>Submit</button>
-      </form>
+      </RegisterForm>
+      <div>{status}</div>
     </Content>
   );
 };
